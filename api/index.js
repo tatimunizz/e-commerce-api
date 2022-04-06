66106461c228806d95a139f28aaa0d8dd0683e1c
tatimunizz <tati.muniz0@gmail.com>
2022-04-06 16:28:45 -0300

    Init: server config

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+import { logger } from '../config/util/util.js';
+
+
+const PORT = process.env.PORT;
+const server = express();
+
+server.use(morgan('tiny'));
+server.use(express.json());
+
+
+server.listen(PORT, () => {
+    logger.info(`Server running at http://localhost:${PORT}`);
+});
